21f79401719ef76554857eda32112f4a00de4dfc
情野陸 <rikuseino33@gmail.com>
2024-08-10 17:09:59 +0900

    feat: add more accurate energy values calculation

--- my-analysis-tool/p_myfunc_computeEnergyFromProbabilityDist.m
+++ my-analysis-tool/p_myfunc_computeEnergyFromProbabilityDist.m
@@ -21,9 +21,35 @@ function [probability_values, energy_values] = p_myfunc_computeEnergyFromProbabi
         energy_values{i, j} = -(sigma.^2)*log(probability_values{i, j}) / 2;
       else
         probability_values{i, j} = 0;
-        energy_values{i, j} = 0;
+        energy_values{i, j} = inf;
       end
     end
+  end
+
+  tol = 1e-6;  % 収束判定の閾値
+  max_iter = 1000;  % 最大繰り返し回数
+  iter = 0;
+  delta = inf;  % 初期差分
+  while delta > tol && iter < max_iter
+    iter = iter + 1;
+    old_energy_values = energy_values;  % エネルギー値を保存
+    
+    % エネルギーの更新
+    C = log(sum(sum(exp(-2 * cellfun(@double, energy_values) / sigma^2))));
+    for i = 1:num_x1_cells
+      for j = 1:num_x2_cells
+        if probability_values{i, j} > 0
+          energy_values{i, j} = -((sigma.^2)/2) * (log(probability_values{i, j}) +  C);
+        end
+      end
+    end
+    
+    % 収束判定
+    delta = max(max(abs(cellfun(@double, energy_values) - cellfun(@double, old_energy_values))));
+  end
+
+  if iter >= max_iter
+    warning('最大繰り返し回数に達しました。収束しなかった可能性があります。');
   end
 
   % 3次元バー グラフを作成してプロットする
